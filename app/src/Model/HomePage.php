a9cc487bf4bd37c3eccedeffae334749f2743aab
Makreig <mark@i-lateral.com>
2019-11-04 16:07:17 +0000

    fixed sorting categories

--- app/src/Model/HomePage.php
+++ app/src/Model/HomePage.php
@@ -44,7 +44,7 @@ class HomePage extends Page
                 'FeaturedCategories',
                 'Categories',
                 $this->FeaturedCategories()
-            )->setConfig(GridFieldConfig_CatalogueRelated::create(Category::class))
+            )->setConfig($config)
         );
         
         return $fields;
